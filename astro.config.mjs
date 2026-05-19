1116685efe71b7215621bcd308063a24c86712bc
r3ximous <rex_hugo2@hotmail.com>
2026-05-20 00:23:55 +0400

    add astro-relative-links dependency to package.json and astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import { process } from 'zod/v4/core';
+import relativeLinks from 'astro-relative-links';
 
 const SERVER_PORT = 3000;
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 
 const LIVE_URL = 'https://astro-react-tailwindcss-starter.vercel.app';
 
-// @ts-ignore
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes('build');
 
 let BASE_URL = LOCALHOST_URL;
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,5 +22,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), relativeLinks()],
 });
